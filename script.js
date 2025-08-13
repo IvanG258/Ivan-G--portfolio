c9d75015a4ea8709c0b97b6478359680960ec20e
Ivan Guitimela <135559066+IvanG258@users.noreply.github.com>
2025-08-13 23:16:31 +1200

    primeiro commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var menu = document.querySelectorAll('.item')
+
+function selecao(){
+    menu.forEach((item)=>item.classList.remove('activo')) 
+    this.classList.add('activo')
+}
+menu.forEach((item)=>
+    item.addEventListener('click',selecao))
+//criando funcionalidade para o botao hamburguer
+var expand= document.querySelector('#menu')
+var menubtn= document.querySelector('.content')
+
+expand.addEventListener('click', function(){
+    menubtn.classList.toggle('expandir')
+})
