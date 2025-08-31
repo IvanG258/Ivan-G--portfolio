77a1899b5ccf1d223c5ea36e90fed0455b4233ef
Ivan Guitimela <135559066+IvanG258@users.noreply.github.com>
2025-08-31 20:53:13 +1200

    scripts to haandle a slider

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-
-/*var menu = document.querySelectorAll('.item')
+//Estilizacao do item activo
+var menu = document.querySelectorAll('.item')
 
 function selecao(){
     menu.forEach((item)=>item.classList.remove('activo')) 
@@ -7,11 +7,45 @@ function selecao(){
 }
 menu.forEach((item)=>
     item.addEventListener('click',selecao))
-//criando funcionalidade para o botao hamburguer
-var expand= document.querySelector('#menu')
-var menubtn= document.querySelector('.content')
 
-expand.addEventListener('click', function(){
-    menubtn.classList.toggle('expandir')
-})
-*/
+/*------------Estilizacao do slider---------*/
+const Btnprev=document.getElementbyId();
+const Btnnext=document.getElementbyId();
+
+function setCurrentDot(){
+    const dots=document.querySelectorAll('.dot')
+    for(let dot of dots){
+        dot.classList.remove('current')
+    }
+    dots[currentSlide].classList.add('current')
+}
+function controler({target:{id}}){
+    const contentLength=content.children.length;
+    switch (id) {
+        case 'nextbtn':{
+            if(slideProps.scroll+slideProps.width<parseInt()){
+                slideProps.scroll+= slideProps.width;
+            }
+            if(currentSlide<contentLength-1){
+                currentSlide +=1;
+                setCurrentDot()
+            }
+            return slider.scrollLeft = slideProps.scroll;
+        }
+            
+            break;
+            case'previous' :{
+                if(currentSlide>0){
+                    currentSlide -=1;
+                    setCurrentDot()
+                }
+                SlideProps.scroll = slideProps.sroll-slideProps.width<0?0:slideProps.scroll-slideProps.width;
+            }
+            break;
+    
+        default:
+            break;
+            btnNext.addEventListener('click',controler)
+            btnPrev.addEventListener('click', controler)
+    }
+}
